35d6db93d05504f5d2de5f60554dc4a7061eacdc
dheruap <dheruap92@gmail.com>
2023-03-08 09:52:12 +0700

    edit tindakan

--- application/erm/logs/log-2023-03-08.php
+++ application/erm/logs/log-2023-03-08.php
@@ -1,30 +1,73 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
-
-ERROR - 2023-03-08 07:32:47 --> Severity: Notice --> Array to string conversion C:\laragon\www\simrs_vclaim\application\erm\views\erm\rajal\kaji_awal_medis\kaji_awal_medis_form.php 113
-ERROR - 2023-03-08 07:33:07 --> Severity: Notice --> Array to string conversion C:\laragon\www\simrs_vclaim\application\erm\views\erm\rajal\kaji_awal_medis\kaji_awal_medis_form.php 113
-ERROR - 2023-03-08 07:33:11 --> Severity: Notice --> Array to string conversion C:\laragon\www\simrs_vclaim\application\erm\views\erm\rajal\kaji_awal_medis\kaji_awal_medis_form.php 113
-ERROR - 2023-03-08 07:33:23 --> Severity: Notice --> Array to string conversion C:\laragon\www\simrs_vclaim\application\erm\views\erm\rajal\kaji_awal_medis\kaji_awal_medis_form.php 113
-ERROR - 2023-03-08 07:36:39 --> Severity: Notice --> Array to string conversion C:\laragon\www\simrs_vclaim\system\database\DB_query_builder.php 2442
-ERROR - 2023-03-08 07:36:39 --> Query error: Unknown column 'Array' in 'where clause' - Invalid query: DELETE FROM `rj_billing_detail`
+ERROR - 2023-03-08 08:16:48 --> Severity: Notice --> Array to string conversion C:\laragon\www\simrs_vclaim\system\database\DB_query_builder.php 2442
+ERROR - 2023-03-08 08:16:48 --> Query error: Unknown column 'Array' in 'where clause' - Invalid query: DELETE FROM `rj_billing_detail`
 WHERE `rj_billing_id` = Array
-ERROR - 2023-03-08 07:36:53 --> Severity: Notice --> Array to string conversion C:\laragon\www\simrs_vclaim\system\database\DB_query_builder.php 2442
-ERROR - 2023-03-08 07:36:53 --> Query error: Unknown column 'Array' in 'where clause' - Invalid query: DELETE FROM `rj_billing_detail`
+ERROR - 2023-03-08 08:20:44 --> Severity: Notice --> Array to string conversion C:\laragon\www\simrs_vclaim\application\erm\views\erm\rajal\kaji_awal_medis\kaji_awal_medis_form.php 113
+ERROR - 2023-03-08 08:20:50 --> Severity: Notice --> Array to string conversion C:\laragon\www\simrs_vclaim\system\database\DB_query_builder.php 2442
+ERROR - 2023-03-08 08:20:50 --> Query error: Unknown column 'Array' in 'where clause' - Invalid query: DELETE FROM `rj_billing_detail`
 WHERE `rj_billing_id` = Array
-ERROR - 2023-03-08 07:37:18 --> Severity: Notice --> Array to string conversion C:\laragon\www\simrs_vclaim\application\erm\views\erm\rajal\kaji_awal_medis\kaji_awal_medis_form.php 113
-ERROR - 2023-03-08 07:38:14 --> Severity: Notice --> Array to string conversion C:\laragon\www\simrs_vclaim\application\erm\views\erm\rajal\kaji_awal_medis\kaji_awal_medis_form.php 113
-ERROR - 2023-03-08 07:38:36 --> Severity: Notice --> Array to string conversion C:\laragon\www\simrs_vclaim\system\database\DB_query_builder.php 2442
-ERROR - 2023-03-08 07:38:36 --> Query error: Unknown column 'Array' in 'where clause' - Invalid query: DELETE FROM `rj_billing_detail`
+ERROR - 2023-03-08 08:32:01 --> Severity: Notice --> Array to string conversion C:\laragon\www\simrs_vclaim\application\erm\views\erm\rajal\kaji_awal_medis\kaji_awal_medis_form.php 113
+ERROR - 2023-03-08 08:34:03 --> Severity: Notice --> Array to string conversion C:\laragon\www\simrs_vclaim\application\erm\views\erm\rajal\kaji_awal_medis\kaji_awal_medis_form.php 113
+ERROR - 2023-03-08 08:58:58 --> Severity: Notice --> Array to string conversion C:\laragon\www\simrs_vclaim\application\erm\views\erm\rajal\kaji_awal_medis\kaji_awal_medis_form.php 113
+ERROR - 2023-03-08 09:00:28 --> Severity: Notice --> Array to string conversion C:\laragon\www\simrs_vclaim\application\erm\views\erm\rajal\kaji_awal_medis\kaji_awal_medis_form.php 113
+ERROR - 2023-03-08 09:00:32 --> Severity: Notice --> Array to string conversion C:\laragon\www\simrs_vclaim\application\erm\views\erm\rajal\kaji_awal_medis\kaji_awal_medis_form.php 113
+ERROR - 2023-03-08 09:00:36 --> Severity: Notice --> Array to string conversion C:\laragon\www\simrs_vclaim\application\erm\views\erm\rajal\kaji_awal_medis\kaji_awal_medis_form.php 113
+ERROR - 2023-03-08 09:00:38 --> Severity: Notice --> Array to string conversion C:\laragon\www\simrs_vclaim\application\erm\views\erm\rajal\kaji_awal_medis\kaji_awal_medis_form.php 113
+ERROR - 2023-03-08 09:00:46 --> Severity: Notice --> Array to string conversion C:\laragon\www\simrs_vclaim\application\erm\views\erm\rajal\kaji_awal_medis\kaji_awal_medis_form.php 113
+ERROR - 2023-03-08 09:01:09 --> Severity: Notice --> Array to string conversion C:\laragon\www\simrs_vclaim\system\database\DB_query_builder.php 2442
+ERROR - 2023-03-08 09:01:09 --> Query error: Unknown column 'Array' in 'where clause' - Invalid query: DELETE FROM `rj_billing_detail`
 WHERE `rj_billing_id` = Array
-ERROR - 2023-03-08 07:39:46 --> Severity: Notice --> Array to string conversion C:\laragon\www\simrs_vclaim\application\erm\views\erm\rajal\kaji_awal_medis\kaji_awal_medis_form.php 113
-ERROR - 2023-03-08 07:39:49 --> Severity: Notice --> Array to string conversion C:\laragon\www\simrs_vclaim\system\database\DB_query_builder.php 2442
-ERROR - 2023-03-08 07:39:49 --> Query error: Unknown column 'Array' in 'where clause' - Invalid query: DELETE FROM `rj_billing_detail`
+ERROR - 2023-03-08 09:08:46 --> Severity: Notice --> Array to string conversion C:\laragon\www\simrs_vclaim\application\erm\views\erm\rajal\kaji_awal_medis\kaji_awal_medis_form.php 113
+ERROR - 2023-03-08 09:09:29 --> Severity: Notice --> Array to string conversion C:\laragon\www\simrs_vclaim\application\erm\views\erm\rajal\kaji_awal_medis\kaji_awal_medis_form.php 113
+ERROR - 2023-03-08 09:09:35 --> Severity: Notice --> Array to string conversion C:\laragon\www\simrs_vclaim\system\database\DB_query_builder.php 2442
+ERROR - 2023-03-08 09:09:35 --> Query error: Unknown column 'Array' in 'where clause' - Invalid query: DELETE FROM `rj_billing_detail`
 WHERE `rj_billing_id` = Array
-ERROR - 2023-03-08 07:40:04 --> Severity: Notice --> Array to string conversion C:\laragon\www\simrs_vclaim\application\erm\views\erm\rajal\kaji_awal_medis\kaji_awal_medis_form.php 113
-ERROR - 2023-03-08 07:40:21 --> Severity: Notice --> Array to string conversion C:\laragon\www\simrs_vclaim\application\erm\views\erm\rajal\kaji_awal_medis\kaji_awal_medis_form.php 113
-ERROR - 2023-03-08 07:40:24 --> Severity: Notice --> Array to string conversion C:\laragon\www\simrs_vclaim\system\database\DB_query_builder.php 2442
-ERROR - 2023-03-08 07:40:24 --> Query error: Unknown column 'Array' in 'where clause' - Invalid query: DELETE FROM `rj_billing_detail`
+ERROR - 2023-03-08 09:09:39 --> Severity: Notice --> Array to string conversion C:\laragon\www\simrs_vclaim\application\erm\views\erm\rajal\kaji_awal_medis\kaji_awal_medis_form.php 113
+ERROR - 2023-03-08 09:09:45 --> Severity: Notice --> Array to string conversion C:\laragon\www\simrs_vclaim\system\database\DB_query_builder.php 2442
+ERROR - 2023-03-08 09:09:45 --> Query error: Unknown column 'Array' in 'where clause' - Invalid query: DELETE FROM `rj_billing_detail`
 WHERE `rj_billing_id` = Array
-ERROR - 2023-03-08 07:41:49 --> Severity: Notice --> Array to string conversion C:\laragon\www\simrs_vclaim\application\erm\views\erm\rajal\kaji_awal_medis\kaji_awal_medis_form.php 113
-ERROR - 2023-03-08 07:41:53 --> Severity: Notice --> Array to string conversion C:\laragon\www\simrs_vclaim\system\database\DB_query_builder.php 2442
-ERROR - 2023-03-08 07:41:53 --> Query error: Unknown column 'Array' in 'where clause' - Invalid query: DELETE FROM `rj_billing_detail`
-WHERE `rj_billing_id` = Array
+ERROR - 2023-03-08 09:10:43 --> Severity: Notice --> Array to string conversion C:\laragon\www\simrs_vclaim\application\erm\views\erm\rajal\kaji_awal_medis\kaji_awal_medis_form.php 113
+ERROR - 2023-03-08 09:11:25 --> Severity: Notice --> Array to string conversion C:\laragon\www\simrs_vclaim\application\erm\views\erm\rajal\kaji_awal_medis\kaji_awal_medis_form.php 113
+ERROR - 2023-03-08 09:11:47 --> Severity: Notice --> Array to string conversion C:\laragon\www\simrs_vclaim\application\erm\views\erm\rajal\kaji_awal_medis\kaji_awal_medis_form.php 113
+ERROR - 2023-03-08 09:12:39 --> Severity: Notice --> Array to string conversion C:\laragon\www\simrs_vclaim\application\erm\views\erm\rajal\kaji_awal_medis\kaji_awal_medis_form.php 113
+ERROR - 2023-03-08 09:14:17 --> Severity: Notice --> Array to string conversion C:\laragon\www\simrs_vclaim\application\erm\views\erm\rajal\kaji_awal_medis\kaji_awal_medis_form.php 113
+ERROR - 2023-03-08 09:15:48 --> Severity: Notice --> Array to string conversion C:\laragon\www\simrs_vclaim\application\erm\views\erm\rajal\kaji_awal_medis\kaji_awal_medis_form.php 113
+ERROR - 2023-03-08 09:18:45 --> Severity: Notice --> Array to string conversion C:\laragon\www\simrs_vclaim\application\erm\views\erm\rajal\kaji_awal_medis\kaji_awal_medis_form.php 113
+ERROR - 2023-03-08 09:19:32 --> Severity: Notice --> Array to string conversion C:\laragon\www\simrs_vclaim\application\erm\views\erm\rajal\kaji_awal_medis\kaji_awal_medis_form.php 113
+ERROR - 2023-03-08 09:20:30 --> Severity: Notice --> Array to string conversion C:\laragon\www\simrs_vclaim\application\erm\views\erm\rajal\kaji_awal_medis\kaji_awal_medis_form.php 113
+ERROR - 2023-03-08 09:20:53 --> Severity: Notice --> Array to string conversion C:\laragon\www\simrs_vclaim\application\erm\views\erm\rajal\kaji_awal_medis\kaji_awal_medis_form.php 113
+ERROR - 2023-03-08 09:21:20 --> Severity: Notice --> Array to string conversion C:\laragon\www\simrs_vclaim\application\erm\views\erm\rajal\kaji_awal_medis\kaji_awal_medis_form.php 113
+ERROR - 2023-03-08 09:21:44 --> Severity: Notice --> Array to string conversion C:\laragon\www\simrs_vclaim\application\erm\views\erm\rajal\kaji_awal_medis\kaji_awal_medis_form.php 113
+ERROR - 2023-03-08 09:27:49 --> Severity: Notice --> Array to string conversion C:\laragon\www\simrs_vclaim\application\erm\views\erm\rajal\kaji_awal_medis\kaji_awal_medis_form.php 113
+ERROR - 2023-03-08 09:27:49 --> Severity: Notice --> Undefined variable: gt C:\laragon\www\simrs_vclaim\application\erm\views\erm\rajal\billing_tindakan\billing_tindakan_form.php 47
+ERROR - 2023-03-08 09:27:49 --> Severity: Notice --> Trying to get property 'tlId' of non-object C:\laragon\www\simrs_vclaim\application\erm\views\erm\rajal\billing_tindakan\billing_tindakan_form.php 47
+ERROR - 2023-03-08 09:27:49 --> Severity: Notice --> Undefined variable: gt C:\laragon\www\simrs_vclaim\application\erm\views\erm\rajal\billing_tindakan\billing_tindakan_form.php 48
+ERROR - 2023-03-08 09:27:49 --> Severity: Notice --> Trying to get property 'tlTitle' of non-object C:\laragon\www\simrs_vclaim\application\erm\views\erm\rajal\billing_tindakan\billing_tindakan_form.php 48
+ERROR - 2023-03-08 09:27:49 --> Severity: Notice --> Undefined variable: gt C:\laragon\www\simrs_vclaim\application\erm\views\erm\rajal\billing_tindakan\billing_tindakan_form.php 49
+ERROR - 2023-03-08 09:27:49 --> Severity: Notice --> Trying to get property 'jasaSarana' of non-object C:\laragon\www\simrs_vclaim\application\erm\views\erm\rajal\billing_tindakan\billing_tindakan_form.php 49
+ERROR - 2023-03-08 09:27:49 --> Severity: Notice --> Undefined variable: gt C:\laragon\www\simrs_vclaim\application\erm\views\erm\rajal\billing_tindakan\billing_tindakan_form.php 50
+ERROR - 2023-03-08 09:27:49 --> Severity: Notice --> Trying to get property 'jasaPelayanan' of non-object C:\laragon\www\simrs_vclaim\application\erm\views\erm\rajal\billing_tindakan\billing_tindakan_form.php 50
+ERROR - 2023-03-08 09:27:49 --> Severity: Notice --> Undefined variable: gt C:\laragon\www\simrs_vclaim\application\erm\views\erm\rajal\billing_tindakan\billing_tindakan_form.php 51
+ERROR - 2023-03-08 09:27:49 --> Severity: Notice --> Trying to get property 'jasaPelayanan' of non-object C:\laragon\www\simrs_vclaim\application\erm\views\erm\rajal\billing_tindakan\billing_tindakan_form.php 51
+ERROR - 2023-03-08 09:28:03 --> Severity: Notice --> Array to string conversion C:\laragon\www\simrs_vclaim\application\erm\views\erm\rajal\rajal_index.php 132
+ERROR - 2023-03-08 09:28:03 --> Severity: Notice --> Array to string conversion C:\laragon\www\simrs_vclaim\application\erm\views\erm\rajal\kaji_awal_medis\kaji_awal_medis_form.php 113
+ERROR - 2023-03-08 09:28:03 --> Severity: Notice --> Undefined variable: gt C:\laragon\www\simrs_vclaim\application\erm\views\erm\rajal\billing_tindakan\billing_tindakan_form.php 47
+ERROR - 2023-03-08 09:28:03 --> Severity: Notice --> Trying to get property 'tlId' of non-object C:\laragon\www\simrs_vclaim\application\erm\views\erm\rajal\billing_tindakan\billing_tindakan_form.php 47
+ERROR - 2023-03-08 09:28:03 --> Severity: Notice --> Undefined variable: gt C:\laragon\www\simrs_vclaim\application\erm\views\erm\rajal\billing_tindakan\billing_tindakan_form.php 48
+ERROR - 2023-03-08 09:28:03 --> Severity: Notice --> Trying to get property 'tlTitle' of non-object C:\laragon\www\simrs_vclaim\application\erm\views\erm\rajal\billing_tindakan\billing_tindakan_form.php 48
+ERROR - 2023-03-08 09:28:03 --> Severity: Notice --> Undefined variable: gt C:\laragon\www\simrs_vclaim\application\erm\views\erm\rajal\billing_tindakan\billing_tindakan_form.php 49
+ERROR - 2023-03-08 09:28:03 --> Severity: Notice --> Trying to get property 'jasaSarana' of non-object C:\laragon\www\simrs_vclaim\application\erm\views\erm\rajal\billing_tindakan\billing_tindakan_form.php 49
+ERROR - 2023-03-08 09:28:03 --> Severity: Notice --> Undefined variable: gt C:\laragon\www\simrs_vclaim\application\erm\views\erm\rajal\billing_tindakan\billing_tindakan_form.php 50
+ERROR - 2023-03-08 09:28:03 --> Severity: Notice --> Trying to get property 'jasaPelayanan' of non-object C:\laragon\www\simrs_vclaim\application\erm\views\erm\rajal\billing_tindakan\billing_tindakan_form.php 50
+ERROR - 2023-03-08 09:28:03 --> Severity: Notice --> Undefined variable: gt C:\laragon\www\simrs_vclaim\application\erm\views\erm\rajal\billing_tindakan\billing_tindakan_form.php 51
+ERROR - 2023-03-08 09:28:03 --> Severity: Notice --> Trying to get property 'jasaPelayanan' of non-object C:\laragon\www\simrs_vclaim\application\erm\views\erm\rajal\billing_tindakan\billing_tindakan_form.php 51
+ERROR - 2023-03-08 09:28:19 --> Severity: Notice --> Array to string conversion C:\laragon\www\simrs_vclaim\application\erm\views\erm\rajal\kaji_awal_medis\kaji_awal_medis_form.php 113
+ERROR - 2023-03-08 09:28:19 --> Severity: Notice --> Undefined variable: gt C:\laragon\www\simrs_vclaim\application\erm\views\erm\rajal\billing_tindakan\billing_tindakan_form.php 47
+ERROR - 2023-03-08 09:28:19 --> Severity: Notice --> Trying to get property 'tlId' of non-object C:\laragon\www\simrs_vclaim\application\erm\views\erm\rajal\billing_tindakan\billing_tindakan_form.php 47
+ERROR - 2023-03-08 09:28:19 --> Severity: Notice --> Undefined variable: gt C:\laragon\www\simrs_vclaim\application\erm\views\erm\rajal\billing_tindakan\billing_tindakan_form.php 48
+ERROR - 2023-03-08 09:28:19 --> Severity: Notice --> Trying to get property 'tlTitle' of non-object C:\laragon\www\simrs_vclaim\application\erm\views\erm\rajal\billing_tindakan\billing_tindakan_form.php 48
+ERROR - 2023-03-08 09:28:19 --> Severity: Notice --> Undefined variable: gt C:\laragon\www\simrs_vclaim\application\erm\views\erm\rajal\billing_tindakan\billing_tindakan_form.php 49
+ERROR - 2023-03-08 09:28:19 --> Severity: Notice --> Trying to get property 'jasaSarana' of non-object C:\laragon\www\simrs_vclaim\application\erm\views\erm\rajal\billing_tindakan\billing_tindakan_form.php 49
+ERROR - 2023-03-08 09:28:19 --> Severity: Notice --> Undefined variable: gt C:\laragon\www\simrs_vclaim\application\erm\views\erm\rajal\billing_tindakan\billing_tindakan_form.php 50
+ERROR - 2023-03-08 09:28:19 --> Severity: Notice --> Trying to get property 'jasaPelayanan' of non-object C:\laragon\www\simrs_vclaim\application\erm\views\erm\rajal\billing_tindakan\billing_tindakan_form.php 50
+ERROR - 2023-03-08 09:28:19 --> Severity: Notice --> Undefined variable: gt C:\laragon\www\simrs_vclaim\application\erm\views\erm\rajal\billing_tindakan\billing_tindakan_form.php 51
+ERROR - 2023-03-08 09:28:19 --> Severity: Notice --> Trying to get property 'jasaPelayanan' of non-object C:\laragon\www\simrs_vclaim\application\erm\views\erm\rajal\billing_tindakan\billing_tindakan_form.php 51
